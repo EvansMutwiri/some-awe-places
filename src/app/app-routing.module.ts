6dd0cfb89ce17a7c3767d4464d930646ee500863
EvansMutwiri <mutwirievansm@gmail.com>
2022-01-25 09:50:35 +0300

    error handling 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { AddNewComponent } from './create/add-new/add-new.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //only redirect when the path is exactly blank
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'create', component: AddNewComponent},
+  {path: '404', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    this.router.errorHandler = (error: any) => {
+      this.router.navigate(['/404']);
+    };
+  }
+ }
