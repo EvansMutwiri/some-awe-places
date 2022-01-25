7464e455f281618491f3bb00c6e572650a700168
EvansMutwiri <mutwirievansm@gmail.com>
2022-01-25 11:06:11 +0300

    places model define

---
+++ src/app/models/place.model.ts
@@ -0,0 +1,13 @@
+export class Place {
+    id: number | undefined;
+    name: string = "";
+    description: string = "";
+    imageUrl: string = "";
+
+    constructor(id?: number, name: string = "", description: string = "", imageUrl: string = "") {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
